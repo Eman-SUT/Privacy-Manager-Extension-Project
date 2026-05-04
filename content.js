11e33786d363ff27c88f82a64284b4b45da0e337
Eman-SUT <eman230103682@sut.edu.eg>
2026-05-04 03:52:55 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,25 @@
-console.log("Content script loaded on page");
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "getPageText") {
+        sendResponse({ text: document.body.innerText });
+    }
+});
+
+const originalToDataURL = HTMLCanvasElement.prototype.toDataURL;
+HTMLCanvasElement.prototype.toDataURL = function(type) {
+    const ctx = this.getContext('2d');
+    if (ctx) {
+        const imageData = ctx.getImageData(0, 0, 1, 1);
+        imageData.data[0] ^= 1;
+        ctx.putImageData(imageData, 0, 0);
+    }
+    return originalToDataURL.apply(this, arguments);
+};
+
+const originalGetChannelData = AudioBuffer.prototype.getChannelData;
+AudioBuffer.prototype.getChannelData = function() {
+    const data = originalGetChannelData.apply(this, arguments);
+    for (let i = 0; i < data.length; i += 100) {
+        data[i] += Math.random() * 0.0001;
+    }
+    return data;
+};
